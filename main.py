ba533b36bf935ced5035d6defa2a7530de06f0b0
Anwar Husain <a8691258@gmail.com>
2020-11-04 22:01:11 +0530

    ADD - recommended songs in autoplay

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import asyncio
+import json
 import os
 import random
 from pathlib import Path
 
 import discord
+import requests
 import youtube_dl
 from discord.ext import commands
 from discord.utils import get
@@ -13,19 +15,23 @@ from youtubesearchpython import SearchVideos
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 PREFIX = os.getenv('DISCORD_PREFIX')
+YOUTUBE_KEY = os.getenv('YOUTUBE_API')
 
 bot = commands.Bot(command_prefix=PREFIX)
 isPlaying = True
 is_skipping = False
 playlist = []
+loop = asyncio.get_event_loop()
+current_playing_song = ""
 
 
 async def autoplay(ctx):
-    global is_skipping
+    global is_skipping, current_playing_song
     voice = get(bot.voice_clients, guild=ctx.guild)
     while isPlaying:
         if playlist:
             song = playlist.pop()
+            current_playing_song = song[0]
             await ctx.channel.send('Now playing ' + song[1])
             voice.play(discord.FFmpegPCMAudio(f'audio_cache/{song[0]}.webm'))
             while voice.is_playing():
@@ -36,9 +42,8 @@ async def autoplay(ctx):
                 else:
                     await asyncio.sleep(.1)
         else:
-            song = get_random_song().strip('\n')
-            audio = [song, song.split("=")[-1]]
-            await download_file(audio[0], audio[1])
+            video_id = current_playing_song if current_playing_song else get_random_song().strip('\n').split("=")[-1]
+            await get_recommended_song(ctx, video_id)
 
 
 @bot.event
@@ -58,7 +63,6 @@ async def summon(ctx):
 
     channel = ctx.author.voice.channel
     await channel.connect()
-    loop = asyncio.get_event_loop()
     loop.create_task(autoplay(ctx))
     await ctx.channel.send(f'Connected to {channel}')
 
@@ -91,6 +95,20 @@ async def skip_music(ctx):
     is_skipping = True
 
 
+@bot.command(pass_context=True, name='disconnect', help='Disconnect the bot')
+async def disconnect(ctx):
+    if not await check_if_user_connected(ctx):
+        return
+
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    if not voice:
+        await ctx.channel.send('Bot is not connected to voice channel, Please summon the bot first')
+        return
+
+    loop.stop()
+    await voice.disconnect()
+
+
 async def check_if_user_connected(ctx):
     connected = ctx.author.voice
     if not connected:
@@ -106,10 +124,10 @@ async def search_video(channel, search):
         await channel.send('Unable to find any Video')
     else:
         await channel.send('Added to queue ' + search.result()[0][3])
-        await download_file(search.result()[0][2], search.result()[0][1])
+        await download_file(channel, search.result()[0][2], search.result()[0][1])
 
 
-async def download_file(url, key):
+async def download_file(channel, url, key):
     file_path = f'audio_cache/{key}.webm'
 
     if Path(file_path).is_file():
@@ -123,14 +141,32 @@ async def download_file(url, key):
         }
         youtube_dl.YoutubeDL(ydl_opts).download([url])
         playlist.append([key, url])
-    finally:
+        clear_cache()
+    except Exception:
+        await channel.send(str(Exception))
         pass
 
 
 def clear_cache():
-    path, dirs, files = next(os.walk("audio_cache"))
-    if len(files) > 1:
-        print(path)
+    files = next(os.walk("audio_cache"))
+    if len(files) > 100:
+        pass
+
+
+async def get_recommended_song(ctx, key):
+    song = get_random_song().strip('\n')
+    audio = [song, song.split("=")[-1]]
+    url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=" \
+          f"{key}&type=video&key={YOUTUBE_KEY}"
+    response = requests.get(url)
+    if response.ok:
+        data = json.loads(response.content)
+        print(data)
+        if len(data['items']) > 1:
+            video_id = data['items'][1]['id']['videoId']
+            audio = [f"https://www.youtube.com/watch?v={video_id}", video_id]
+
+    await download_file(ctx.channel, audio[0], audio[1])
 
 
 def get_random_song():
